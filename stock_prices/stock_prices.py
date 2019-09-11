e6f718706fce1c103c9abe19368a37d317969397
Krisli Dimo <krislidimo@gmail.com>
2019-09-11 18:00:28 -0500

    implemented find_max_profit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,9 +2,52 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
+#   4
+#  3  -
+#2     2  2
+#       1
 
+#       4
+#   3  3
+#  2  2
+# 1 
+
+# profit 
+# - accending and new low hasn't been hit
+# - only decending => lowest loss
+
+# for every price
+	# curr > high
+		# set high
+	# curr <
+def find_max_profit(prices):
+	max_profit = 0
+	profit = 0
+	low = prices[0]
+	high = prices[0]
+
+	for i in range(len(prices)):
+		current = prices[i]
+
+		# decending
+		if current < low:
+			low = current
+			high = 0 # high set to 0 assures profit only accure when accending
+		# accending
+		elif current > high:
+			high = current
+			profit = high - low
+
+			# only set profit if higher then max profits
+			if profit > max_profit:
+				max_profit = profit
+
+	# if prices only decend, max_profit never gets set
+	if max_profit == 0:
+		max_profit = prices[-1] - prices[-2]
+
+	print(max_profit)
+	return max_profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
